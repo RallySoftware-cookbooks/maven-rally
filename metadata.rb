78c47b26dc118d8618aad1d696190489d810867d
Jonathan Chauncey <jchauncey@rallydev.com>
2013-09-30 10:49:13 -0600

    updating maven-rally metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,5 @@ description      'Installs/Configures maven'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe 'chromedriver::default', 'Installs chromedriver'
-
 supports 'centos'
 supports 'ubuntu'
