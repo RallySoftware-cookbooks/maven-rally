f93876ce694fa7feac4c4013e83bebe994f220a8
Matt Farrar <mfarrar@rallydev.com>
2013-11-23 15:55:35 -0700

    Added some more unit tests

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -2,20 +2,28 @@ require_relative 'spec_helper'
 
 describe 'maven-rally::default' do
   let(:version) { 'foo' }
-  let(:install_dir) { '/usr/local' }
-  let(:tar_gz) { "apache-maven-#{version}-bin.tar.gz" }
-  let(:tar_gz_path) { "#{install_dir}/#{tar_gz}" }
+  let(:base_dir) { '/foo/bar' }
+  let(:maven_file_name) { "apache-maven-#{version}" }
+  let(:install_path) { "#{base_dir}/#{maven_file_name}" }
+  let(:archive) { "#{maven_file_name}-bin.tar.gz" }
+  let(:archive_path) { "#{base_dir}/#{archive}" }
+
   subject(:chef_run) do
-    stub_command("test -f #{tar_gz_path}").and_return(true)
+    stub_command("test -f #{archive_path}").and_return(true)
     ChefSpec::Runner.new do |node|
       node.set[:maven][:version] = version
+      node.set[:maven][:path] = base_dir
     end.converge described_recipe
   end
 
-  it { should create_remote_file(tar_gz_path) }
+  it { should create_remote_file(archive_path) }
 
-  it { should run_execute("tar xzf #{tar_gz}").with(cwd: install_dir) }
+  it { should run_execute("tar xzf #{archive}").with(cwd: base_dir) }
+  
+  it { should create_link("#{base_dir}/maven") }
+  specify { chef_run.link("#{base_dir}/maven").should link_to(install_path) }
 
-
+  it { should create_link('/usr/local/bin/mvn') }
+  specify { chef_run.link('/usr/local/bin/mvn').should link_to("#{base_dir}/maven/bin/mvn") }
 
 end
